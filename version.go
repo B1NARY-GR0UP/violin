8f1400d94ff2cdad44316bc2ddb50419ec976487
Lorain <743804605@qq.com>
2023-06-30 19:09:26 +0800

    chore: release v0.2.0

--- version.go
+++ version.go
@@ -18,5 +18,5 @@ package violin
 // BINARY-VIOLIN
 const (
 	Name    = "VIOLIN"
-	Version = "v0.1.1"
+	Version = "v0.2.0"
 )
--- version_test.go
+++ version_test.go
@@ -23,5 +23,5 @@ import (
 
 func TestVersion(t *testing.T) {
 	assert.Equal(t, "VIOLIN", Name)
-	assert.Equal(t, "v0.1.1", Version)
+	assert.Equal(t, "v0.2.0", Version)
 }
